f93a524a64eaf1390e4f6df86521e85dba18a62b
Kenny Ho <kenny.ho5825@gmail.com>
2024-02-26 22:14:57 -0600

    edit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly on kenny.ho5825@gmail.com"
       );
     });
 }
